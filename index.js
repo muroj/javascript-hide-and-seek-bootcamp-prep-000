70be8da7a273664e09c490600ab75e1c0615c299
Joe Muro <jmuro0321@gmail.com>
2018-08-05 01:26:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return getFirstSelector(`#nested .target`);
+}
+
+function increaseRankBy(n) {
+  var rlNodes = document.querySelectorAll('.ranked-list li')
+  
+  for (let i = 0; i < rlNodes.length; i++) {
+    rlNodes[i].innerHTML = parseInt(rlNodes[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var grande = document.querySelector('#grand-node');
+  
+  for (let i = 0; i < grande.) {
+    
+  }
+  
+}
