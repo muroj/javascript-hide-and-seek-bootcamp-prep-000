187d0dc987ab6f9c602ce08fc5476dc1d14398f2
Joe Muro <jmuro0321@gmail.com>
2018-08-05 01:48:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return getFirstSelector(`#nested .target`);
+}
+
+function increaseRankBy(n) {
+  var rlNodes = document.querySelectorAll('.ranked-list li')
+  
+  for (let i = 0; i < rlNodes.length; i++) {
+    rlNodes[i].innerHTML = parseInt(rlNodes[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  
+  var currentNode = document.querySelector('#grand-node');
+  var prevNode = null;
+  
+  while (currentNode) {
+    prevNode = currentNode;
+    currentNode = currentNode.querySelector('div');
+  }
+  
+  return prevNode;
+}
